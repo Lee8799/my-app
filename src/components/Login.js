d99c1478531a0c6d158fc67e513167b75a4a69ea
Leroy Williams <ljwilliams434@gmail.com>
2022-05-04 17:42:39 -0400

    May the forth..

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,12 +6,13 @@ import { Link } from 'react-router-dom';
 function Login(props) {
   const [username, setUsername] = useState ('');
   const [password, setPassword] = useState ('');
+ 
     const setLogin = props.logIn
 
   console.log('username:',username)
   console.log('password:',password)
   const handleSubmit = async (event) => {
-    // event.preventdefault ();
+    
     await fetch ('https://strangers-things.herokuapp.com/api/2110-vpi-web-pt/users/login', {
         method: "POST",
         headers: {
@@ -36,12 +37,15 @@ function Login(props) {
         <h3>Log In</h3>
         <form onSubmit={handleSubmit}>
                 <label htmlFor='username'>Username:</label>
-                <input type='text' required name='username' value={username} onChange={(event) => setUsername(event.target.value)} />
+                <input type='text' required name='username' value={username}
+                 onChange={(event) => setUsername(event.target.value)} />
                 <label htmlFor='password'>Password:</label>
                 <input type='password' required minLength='4' name='password' value={password} onChange={(event) => setPassword(event.target.value)} />
                 <button type='submit'>Log In</button>
-            </form>
+                reload(Login);
+        </form>
             <Link to="/Register">Don't have an account yet? Register today!</Link>
+
     </div>
   )
 }
